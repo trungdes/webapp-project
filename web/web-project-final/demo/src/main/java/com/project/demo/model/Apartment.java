8ae04be1d1b8ac6ae2f33ffa4aae62bea7762955
trungdes <ITCSIU22260@student.hcmiu.edu.vn>
2025-05-25 18:38:38 +0700

    update css

--- web/web-project-final/demo/src/main/java/com/project/demo/model/Apartment.java
+++ web/web-project-final/demo/src/main/java/com/project/demo/model/Apartment.java
@@ -13,20 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-<<<<<<< HEAD
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-=======
->>>>>>> cuong
+
 
 @Data
 @Entity
--- web/web-project-final/demo/src/main/java/com/project/demo/service/ApartmentService.java
+++ web/web-project-final/demo/src/main/java/com/project/demo/service/ApartmentService.java
@@ -4,14 +4,9 @@ import java.util.List;
 
 import com.project.demo.dto.ApartmentDto;
 import com.project.demo.model.Apartment;
-<<<<<<< HEAD
 
 public interface ApartmentService {
     List<ApartmentDto> findAllApartments();
 
-=======
-public interface ApartmentService {
-    List<ApartmentDto> findAllApartments();
->>>>>>> cuong
     Apartment saveApartment(Apartment apartment);
 }
